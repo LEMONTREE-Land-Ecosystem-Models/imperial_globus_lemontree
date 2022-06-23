f4d01c5a5ff0a1cf81c5219c8c0e6aa7d0f1559d
dorme <d.orme@imperial.ac.uk>
2022-06-23 11:37:45 +0100

    Extend SNU Ryu conversion to include output directory and uint16 packing

--- source/SNU_Ryu_FPAR_LAI/convert_SNU_Ryu_to_netcdf.pbs.sh
+++ source/SNU_Ryu_FPAR_LAI/convert_SNU_Ryu_to_netcdf.pbs.sh
@@ -11,13 +11,16 @@
 #PBS -j oe
 #PBS -o /rds/general/project/lemontree/live/source/SNU_Ryu_FPAR_LAI/conversion_^job_id^_^array_index^.out
 
-# Submit using qsub -v SCRIPT_VAR=FPAR or qsub -v SCRIPT_VAR=LAI
+# Submit using qsub -v SCRIPT_VAR=FPAR or qsub -v SCRIPT_VAR=LAI and 
+# * Use SCRIPT_OUTDIR_SUFFIX to set the suffix on the output dir,
+#   e.g. SCRIPT_OUTDIR_SUFFIX=in_here -> FPAR_in_here
+# * Use SCRIPT_PACK=1 to pack outputs into uint16
 
 module load anaconda3/personal
 
 source activate base
 
-echo "In PBS.SH and running $SCRIPT_VAR : $PBS_ARRAY_INDEX"
+echo "In PBS.SH and running:\n VAR: $SCRIPT_VAR\n OUT_DIR_SUFFIX: $SCRIPT_OUTDIR_SUFFIX$\n ARR_IND: $PBS_ARRAY_INDEX\n ${SCRIPT_PACK:-Not set}"
 
 python /rds/general/project/lemontree/live/source/SNU_Ryu_FPAR_LAI/convert_SNU_Ryu_to_netcdf.py
 
--- source/SNU_Ryu_FPAR_LAI/convert_SNU_Ryu_to_netcdf.py
+++ source/SNU_Ryu_FPAR_LAI/convert_SNU_Ryu_to_netcdf.py
@@ -8,40 +8,51 @@ import numpy as np
 import xarray
 
 # TODO - look at gathering to save space - cf-python implements reading and
-#        unpacking back to 2D really elegantly but xarray and netcdf4 read the 
+#        unpacking back to 2D really elegantly but xarray and netcdf4 read the
 #        fine but need unpacking separately (not built in)
 
-# Needs to be submitted with an array job to loop over years and 
-# specify the variable via an environment variable SCRIPT_VAR
+# Needs to be submitted with an array job to loop over years with env variables:
+# * variable name in SCRIPT_VAR
+# * output dir suffix in SCRIPT_OUTDIR_SUFFIX
+# * optional packing flag in SCRIPT_PACK, packs the float into uint16
 
-var = os.getenv('SCRIPT_VAR')
-arrind = os.getenv('PBS_ARRAY_INDEX')
+var = os.getenv("SCRIPT_VAR")
+out_dir_suffix = os.getenv("SCRIPT_OUTDIR_SUFFIX")
+pack = bool(os.getenv("SCRIPT_PACK")) | False
+
+arrind = os.getenv("PBS_ARRAY_INDEX")
 year = 1999 + int(arrind)
 
-sys.stdout.write(f"In Py and running: {var} {arrind} {year}")
+sys.stdout.write(
+    f"In Py and running:\n  VAR: {var}\n" f"  YEAR: {year}\n  PACKING: {pack}"
+)
 
 # var should be one of FPAR or LAI
-if var == 'FPAR':
-    dir_path = 'source_format/fPAR_daily_0.05deg'
-    file_glob = f'FPAR_Daily^.{year}*.mat'
-    unit = 'XXX'
-elif var == 'LAI':
-    dir_path = 'source_format/LAI_daily_0.05deg'
-    file_glob = f'LAI_Daily^.{year}*.mat'
-    unit='XXX'
+if var == "FPAR":
+    canonical_name = "fraction_of_surface_downwelling_photosynthetic_radiative_flux_absorbed_by_vegetation"
+    dir_path = "source_format/fPAR_daily_0.05deg"
+    file_glob = f"FPAR_Daily^.{year}*.mat"
+    scale_factor = 1 / 64000  # Mapping 0 - 1 into 0 - 64000
+    unit = "1"
+elif var == "LAI":
+    canonical_name = "leaf_area_index"
+    dir_path = "source_format/LAI_daily_0.05deg"
+    file_glob = f"LAI_Daily^.{year}*.mat"
+    scale_factor = 1 / 6400  # Mapping 0 - 10 into 0 - 64000
+    unit = "1"
 else:
-    sys.stderr.write('Unknown or missing SCRIPT_VAR value')
+    sys.stderr.write("Unknown or missing SCRIPT_VAR value")
     sys.exit()
 
 # Location of the root directory
-dir_root = '/rds/general/project/lemontree/live/source/SNU_Ryu_FPAR_LAI/'
+dir_root = "/rds/general/project/lemontree/live/source/SNU_Ryu_FPAR_LAI/"
 
-# Get the files 
+# Get the files
 input_file_pattern = os.path.join(dir_root, dir_path, file_glob)
 input_year_files = glob.glob(input_file_pattern)
 
 # Get the day of year and convert to date
-days = [int(f.split('.')[-2]) for f in input_year_files]
+days = [int(f.split(".")[-2]) for f in input_year_files]
 
 # Get a list of zero-based indices for iterating over days in base
 # matrix (might not start Jan 1st)
@@ -51,46 +62,69 @@ day_ord = np.argsort(np.argsort(days))
 # overrun at end of sequence to avoid clipping last value
 res = 0.05
 longitude = np.arange(-180 + res / 2, 180, res)
-latitude = np.arange(90 - res/2, -90, -res)
+latitude = np.arange(90 - res / 2, -90, -res)
 
 # Get the landmask for unpacking data - note that this is (lat, long)
-landmask = sio.loadmat(os.path.join(dir_root, 'source_format/Landmask.005d.mat'))['data']
+landmask = sio.loadmat(os.path.join(dir_root, "source_format/Landmask.005d.mat"))[
+    "data"
+]
 
 # The issue here is that the packing of the values into the daily files
 # uses column major ordering, not row major, so need to do some shuffling
 # to get array indices that will insert the data into the grid correctly.
-land_cell_idx_col_major = np.nonzero(landmask.flatten(order='F'))
-land_cell_idx = np.unravel_index(land_cell_idx_col_major, landmask.shape, order='F')
+land_cell_idx_col_major = np.nonzero(landmask.flatten(order="F"))
+land_cell_idx = np.unravel_index(land_cell_idx_col_major, landmask.shape, order="F")
 
 # Make a 3D array to complete for the year following CF TZYX recommendation
-base_grid = np.ndarray((len(days), len(latitude), len(longitude)), 
-                       dtype='float32')
+base_grid = np.ndarray((len(days), len(latitude), len(longitude)), dtype="float32")
 
 # Loop over the files
 for day_idx, this_file in zip(day_ord, input_year_files):
-    
+
     # Extract and unpack
     mat = sio.loadmat(this_file)
-    mat_data = mat['data']
+    mat_data = mat["data"]
 
     # insert the values into an empty matrix
-    mat_data_unpack = np.empty_like(landmask, dtype='float32')
+    mat_data_unpack = np.empty_like(landmask, dtype="float32")
     mat_data_unpack[:] = np.nan
     mat_data_unpack[land_cell_idx] = mat_data.flatten()
-    
+
     # insert into the correct day of year
     base_grid[day_idx, :, :] = mat_data_unpack
 
 # Create the xarray object holding the data
-dates = sorted([datetime.datetime(year, 1, 1) + datetime.timedelta(d - 1) for d in days])
-xds = xarray.DataArray(base_grid, 
-                       coords=[dates, latitude, longitude], 
-                       dims=['time', 'latitude', 'longitude'])
-
-xds.name = var
+dates = sorted(
+    [datetime.datetime(year, 1, 1) + datetime.timedelta(d - 1) for d in days]
+)
+xds = xarray.DataArray(
+    base_grid,
+    coords=[dates, latitude, longitude],
+    dims=["time", "latitude", "longitude"],
+    name=canonical_name,
+    attrs={"units": unit},
+)
 
 # Save to disk - creating output directory
-out_dir = os.path.join(dir_root, f'{var}_netcdf_cf')
+out_dir = os.path.join(dir_root, f"{var}_{out_dir_suffix}")
 os.makedirs(out_dir, exist_ok=True)
-out_file = os.path.join(out_dir, f'{var}_{year}.nc')
-xds.to_netcdf(out_file, encoding={var: {'zlib': True, 'complevel': 6}})
+out_file = os.path.join(out_dir, f"{var}_{year}.nc")
+
+# Set up the output encoding - compress either output but
+# with pack set to True, configures the packing into uint16
+
+if pack:
+    encoding = {
+        canonical_name: {
+            "zlib": True,
+            "complevel": 6,
+            "dtype": "uint16",
+            "scale_factor": scale_factor,
+            "_FillValue": 65535,
+        }
+    }
+else:
+    encoding = {canonical_name: {"zlib": True, "complevel": 6}}
+
+
+xds.to_netcdf(out_file, encoding=encoding)
