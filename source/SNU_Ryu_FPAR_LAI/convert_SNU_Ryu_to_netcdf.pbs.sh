a2f92e87bffaf5687d6e2ce996ad1f34e5168212
David Orme <d.orme@imperial.ac.uk>
2022-06-23 13:11:07 +0100

    SNU Ryu: shorter script var names, tidied outfile reporting, variation in sourcefile names

--- source/SNU_Ryu_FPAR_LAI/convert_SNU_Ryu_to_netcdf.pbs.sh
+++ source/SNU_Ryu_FPAR_LAI/convert_SNU_Ryu_to_netcdf.pbs.sh
@@ -7,20 +7,25 @@
 
 #PBS -lselect=1:ncpus=1:mem=96gb
 #PBS -lwalltime=24:00:00
-#PBS -J 1-20
+#PBS -J 1-21
 #PBS -j oe
-#PBS -o /rds/general/project/lemontree/live/source/SNU_Ryu_FPAR_LAI/conversion_^job_id^_^array_index^.out
+#PBS -o /rds/general/project/lemontree/live/source/SNU_Ryu_FPAR_LAI/conversion_^array_index^.out
 
-# Submit using qsub -v SCRIPT_VAR=FPAR or qsub -v SCRIPT_VAR=LAI and 
-# * Use SCRIPT_OUTDIR_SUFFIX to set the suffix on the output dir,
-#   e.g. SCRIPT_OUTDIR_SUFFIX=in_here -> FPAR_in_here
-# * Use SCRIPT_PACK=1 to pack outputs into uint16
+# Env vars needed:
+#
+# * Use VAR=FPAR or VAR=LAI to set the variable 
+# * Use OUTDIR_SUFFIX to set the suffix on the output dir,
+#        e.g. OUTDIR_SUFFIX=in_here -> FPAR_in_here
+# * Optionally use PACK=1 to pack outputs into uint16
+#
+# Example:
+# qsub -v VAR=FPAR,OUTDIR_SUFFIX=test,PACK=1 convert_SNU_Ryu_to_netcdf.pbs.sh 
 
 module load anaconda3/personal
 
 source activate base
 
-echo "In PBS.SH and running:\n VAR: $SCRIPT_VAR\n OUT_DIR_SUFFIX: $SCRIPT_OUTDIR_SUFFIX$\n ARR_IND: $PBS_ARRAY_INDEX\n ${SCRIPT_PACK:-Not set}"
+echo -e "In PBS.SH and running:\n VAR: $VAR\n  OUTDIR_SUFFIX: $OUTDIR_SUFFIX\n  ARR_IND:  $PBS_ARRAY_INDEX\n  PACK: ${PACK:-Not set}"
 
 python /rds/general/project/lemontree/live/source/SNU_Ryu_FPAR_LAI/convert_SNU_Ryu_to_netcdf.py
 
