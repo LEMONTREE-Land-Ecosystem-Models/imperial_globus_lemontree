82cf290f58398f04dab15b7abde2d1884d3e632e
dorme <d.orme@imperial.ac.uk>
2022-09-16 13:15:38 +0100

    Updating SNU year completeness checker

--- source/SNU_Ryu_FPAR_LAI/check_SNU_Ryu_days.py
+++ source/SNU_Ryu_FPAR_LAI/check_SNU_Ryu_days.py
@@ -1,29 +1,47 @@
+from itertools import groupby
 import os
-import sys
-import glob
+import re
+from pathlib import Path
 
 """
 This script was used to locate missing days with the original SNU_Ryu data files
-for FPAR and LAI. Flat directory of 20 years of daily files for each variable.
+for FPAR and LAI. Directories with aribtrary nesting of 20 years of daily files 
+for each variable.
 """
 
-for var in ('FPAR', 'LAI'):
-    for year in range(2000,2020):
-        # var should be one of FPAR or LAI
-        if var == 'FPAR':
-            dir_path = 'fPAR_daily_0.05deg'
-            file_glob = f'FPAR_Daily^.{year}*'
-        elif var == 'LAI':
-            dir_path = 'LAI_daily_0.05deg'
-            file_glob = f'LAI_Daily^.{year}*'
-        
-        # Location of the root directory
-        dir_root = '/rds/general/project/lemontree/live/incoming/SNU_Ryu/'
-        
-        input_file_pattern = os.path.join(dir_root, dir_path, file_glob)
-        input_year_files = glob.glob(input_file_pattern)
-        
-        # Get the days and missing days
-        days = sorted([int(f.split('.')[3]) for f in input_year_files])
-        missing = [str(d) for d in range(1, 367) if d not in days]
-        print(f'{var} {year} {len(input_year_files)} found. Missing: {",".join(missing)}')
+# Location of the root directory
+dir_root = '/rds/general/project/lemontree/ephemeral/SNU_data_sharing_Sep_2022/'
+
+# Regex for the file date
+yr_regex = re.compile('A([0-9]{4})([0-9]{3})')
+
+# Hard code leap years
+leap = [1972, 1976, 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2016, 2020]
+
+# Loop over variables
+for var in ('FPAR', 'LAI', 'NIRv', 'Rg', 'PAR'):
+
+    # Get the directory path and the file glob
+    var_path = f'{var}_daily_005d_V1'
+    var_glob = f'{var}_Daily_005d.*.nc'
+
+    input_file_dir = os.path.join(dir_root, var_path)
+    input_year_files = Path(input_file_dir).rglob(var_glob)
+
+    # Jeepers, this is quick. 15K files almost instantly.
+    year_day = [yr_regex.search(p.name).groups() for p in input_year_files]
+
+    # Sort and group by year
+    year_day.sort()
+    year_groups = groupby(year_day, key=lambda x: x[0])
+
+    for yr, data in year_groups:
+        nday = 366 if int(yr) in leap else 365
+        target = range(1, nday + 1)
+        days = [int(x[1]) for x in data]
+        missing = set(target).difference(days)
+        if missing:
+            missing = ', '.join([str(x) for x in missing])
+            print(f"{var} - {yr}: {missing}")
+        else:
+            print(f"{var} - {yr}: All found")
