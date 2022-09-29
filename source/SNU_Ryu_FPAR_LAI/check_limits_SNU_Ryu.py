66f10a552246956ab488402ba32aac0db7189adb
dorme <d.orme@imperial.ac.uk>
2022-09-29 11:28:48 +0100

    Added NA count to limit checker + bug fix

--- source/SNU_Ryu_FPAR_LAI/check_limits_SNU_Ryu.py
+++ source/SNU_Ryu_FPAR_LAI/check_limits_SNU_Ryu.py
@@ -1,10 +1,10 @@
 import os
-import sys
 import re
+import sys
 from pathlib import Path
 
-import xarray
 import numpy as np
+import xarray
 
 """
 This script is used to check the limits on SNU Ryu incoming files. The
@@ -15,26 +15,26 @@ following environment variables need to be set:
 # Environment variables
 var = os.getenv("VAR")
 
-sys.stdout.write(
-    f"In Py and running:\n  VAR: {var}\n"
-)
+sys.stdout.write(f"In Py and running:\n  VAR: {var}\n")
 sys.stdout.flush()
 
 # Other variables
 # Regex for the file date
-yr_regex = re.compile('A([0-9]{4})([0-9]{3})')
+yr_regex = re.compile("A([0-9]{4})([0-9]{3})")
 
 # Location of the root directory
 dir_root = "/rds/general/project/lemontree/ephemeral/SNU_data_sharing_Sep_2022"
 
 # Variable list
-var_list = {"FPAR": ['FPAR', 'FPAR', -10]
-            "LAI": ["LAI", "LAI", -10],
-            "PAR": ["PAR", "PAR", -10],
-            "Rg": ["Rg", "Rg", -10], 
-            "NIRv": ["NIRv", "NIRv", -1],
-            "MCD43C4": ["NIRv", " MCD43C4 qc", 255]}
-    
+var_list = {
+    "FPAR": ["FPAR", "FPAR", -10],
+    "LAI": ["LAI", "LAI", -10],
+    "PAR": ["PAR", "PAR", -10],
+    "Rg": ["Rg", "Rg", -10],
+    "NIRv": ["NIRv", "NIRv", -1],
+    "MCD43C4": ["NIRv", " MCD43C4 qc", 255],
+}
+
 if var not in var_list:
     raise ValueError(f"Unknown variable: {var}")
 
@@ -42,17 +42,17 @@ file_var, var_name, missing_value = var_list[var]
 
 # Recursive search for all files across years - directory structure is variable - and
 # then filter down to the requested year
-input_file_dir = os.path.join(dir_root, f'{file_var}_daily_005d_V1')
-input_year_files = Path(input_file_dir).rglob(f'{file_var}_Daily_005d.*.nc')
+input_file_dir = os.path.join(dir_root, f"{file_var}_daily_005d_V1")
+input_year_files = Path(input_file_dir).rglob(f"{file_var}_Daily_005d.*.nc")
 
 # Jeepers, this is quick. 15K files almost instantly.
 year_files = [(yr_regex.search(p.name).groups(), p) for p in input_year_files]
 year_files.sort()
 
 # Create an output file
-outfile = os.path.join(dir_root, 'limits', var, f"{var_name}_limits.csv")
+outfile = os.path.join(dir_root, "limits", var, f"{var_name}_limits.csv")
 
-with open(outfile, 'w', buffering=1024) as outf:
+with open(outfile, "w", buffering=1024) as outf:
 
     # Loop over the files
     for (year, day_idx), this_file in year_files:
@@ -63,13 +63,16 @@ with open(outfile, 'w', buffering=1024) as outf:
             mat = ds[var_name]
         except (OSError, RuntimeError, ValueError) as e:
             err_string = str(e).replace("\n", " ")
-            outf.write(f"{year},{day_idx},NA, NA,NA # {err_string}\n")
+            outf.write(f"{year},{day_idx},NA,NA,NA,NA # {err_string}\n")
             continue
 
         # Set NAs and count and remove inf
         mat = mat.where(mat != missing_value)
+        na_count = mat.isnull().sum()
         inf_count = np.isinf(mat).sum()
         mat = mat.where(np.isfinite(mat))
 
         # Report limits
-        outf.write(f"{year},{day_idx},{inf_count},{float(mat.min())},{float(mat.max())}\n")
+        outf.write(
+            f"{year},{day_idx},{na_count},{inf_count},{float(mat.min())},{float(mat.max())}\n"
+        )
