5eb10ec00f3cd4bf3ee656f882c583609932759c
dorme <d.orme@imperial.ac.uk>
2022-09-30 19:49:20 +0100

    Checking on monthly SNU files

---
+++ source/SNU_Ryu_FPAR_LAI/check_limits_SNU_Ryu_monthly.pbs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This script repakages the incoming data from individual day files in landonly
+# vectors to netcdf files containing a year of data in an unpacked grid
+
+# Use the throughput class - single node, single cpu
+
+#PBS -lselect=1:ncpus=1:mem=96gb:gpfs=true
+#PBS -lwalltime=24:00:00
+#PBS -j oe
+#PBS -o limit_checker.out
+
+# Env vars needed:
+#
+# * Use VAR to set the variable 
+
+# Example:
+# qsub -v VAR=FPAR check_limits_SNU_Ryu.pbs.sh 
+
+module load anaconda3/personal
+
+source activate python3.10
+
+python --version
+
+echo -e "In PBS.SH and running:\n VAR: $VAR\n"
+
+python /rds/general/project/lemontree/live/source/SNU_Ryu_FPAR_LAI/check_limits_SNU_Ryu_monthly.py
+
+conda deactivate
---
+++ source/SNU_Ryu_FPAR_LAI/check_limits_SNU_Ryu_monthly.py
@@ -0,0 +1,78 @@
+import os
+import re
+import sys
+from pathlib import Path
+
+import numpy as np
+import xarray
+
+"""
+This script is used to check the limits on SNU Ryu incoming files. The
+following environment variables need to be set:
+* variable name in VAR
+"""
+
+# Environment variables
+var = os.getenv("VAR")
+
+sys.stdout.write(f"In Py and running:\n  VAR: {var}\n")
+sys.stdout.flush()
+
+# Other variables
+# Regex for the file date
+yr_regex = re.compile("A([0-9]{4})([0-9]{2})")
+
+# Location of the root directory
+dir_root = "/rds/general/project/lemontree/ephemeral/SNU_data_sharing_Sep_2022"
+
+# Variable list
+var_list = {
+    "FPAR": ["FPAR", "FPAR", -10],
+    "LAI": ["LAI", "LAI", -10],
+    "PAR": ["PAR", "PAR", -10],
+    "Rg": ["Rg", "Rg", -10],
+    "NIRv": ["NIRv", "NIRv", -1],
+    "MCD43C4": ["NIRv", " MCD43C4 qc", 255],
+}
+
+if var not in var_list:
+    raise ValueError(f"Unknown variable: {var}")
+
+file_var, var_name, missing_value = var_list[var]
+
+# Recursive search for all files across years - directory structure is variable - and
+# then filter down to the requested year
+input_file_dir = os.path.join(dir_root, f"{file_var}_monthly_005d_V1")
+input_year_files = Path(input_file_dir).rglob(f"{file_var}_Monthly_005d.*.nc")
+
+# Jeepers, this is quick. 15K files almost instantly.
+year_files = [(yr_regex.search(p.name).groups(), p) for p in input_year_files]
+year_files.sort()
+
+# Create an output file
+outfile = os.path.join(dir_root, "limits", f"{var_name}_monthly_limits.csv")
+
+with open(outfile, "w", buffering=1024) as outf:
+
+    # Loop over the files
+    for (year, mn_idx), this_file in year_files:
+
+        # Load, reduce to required DataArray and set missing values
+        try:
+            ds = xarray.load_dataset(this_file)
+            mat = ds[var_name]
+        except (OSError, RuntimeError, ValueError) as e:
+            err_string = str(e).replace("\n", " ")
+            outf.write(f"{year},{mn_idx},NA,NA,NA,NA # {err_string}\n")
+            continue
+
+        # Set NAs and count and remove inf
+        mat = mat.where(mat != missing_value)
+        na_count = mat.isnull().sum()
+        inf_count = np.isinf(mat).sum()
+        mat = mat.where(np.isfinite(mat))
+
+        # Report limits
+        outf.write(
+            f"{year},{mn_idx},{na_count},{inf_count},{float(mat.min())},{float(mat.max())}\n"
+        )
