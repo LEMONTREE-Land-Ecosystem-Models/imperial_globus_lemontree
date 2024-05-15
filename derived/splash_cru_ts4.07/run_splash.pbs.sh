df67c80e97b4b8d8045c4660af45ff79b2db3aa4
David Orme <davidorme@users.noreply.github.com>
2024-05-15 17:33:20 +0100

    Updated PBS directives and script for run splash

--- derived/splash_cru_ts4.07/run_splash.pbs.sh
+++ derived/splash_cru_ts4.07/run_splash.pbs.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-# This script runs the potential GPP calculations by year.
+# This script runs the SPLASH model across the CRU TS 4.07 dataset
 
-# NOTES:
-#
-# * If this analysis is reworked to include soil moisture, including splash, then it
-#   will need to run as a single job to iterate over the soil moisture years in turn.
-#
-# * Running a single year may be too short for a sensible job spec, in which case the
-#   array index could be used to loop over decades for the 1901 - 2018 data
-#
-# * Use the throughput class - single node, single cpu, using GPFS for better file
-#   handling
+# Uses the throughput class - single node, single cpu, using GPFS for better file
+# handling,
 
-#PBS -lselect=1:ncpus=8:mem=96gb:gpfs=true
+#PBS -lselect=1:ncpus=1:mem=96gb:gpfs=true
 #PBS -lwalltime=24:00:00
-#PBS -J 1901-2018
 #PBS -j oe
-#PBS -o /rds/general/project/lemontree/ephemeral/potential_gpp_^array_index^.out
+#PBS -o /rds/general/project/lemontree/ephemeral/run_splash.out
 
 
 module load anaconda3/personal
