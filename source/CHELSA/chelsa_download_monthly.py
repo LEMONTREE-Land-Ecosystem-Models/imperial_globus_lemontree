cb64a4908f5d45d90151b6ede99fc397c60502d6
David Orme <davidorme@users.noreply.github.com>
2025-05-21 17:23:28 +0100

    Tidy up file lists in CHELSA

--- source/CHELSA/chelsa_download_monthly.py
+++ source/CHELSA/chelsa_download_monthly.py
@@ -4,9 +4,9 @@ import subprocess
 # Which variables to download - file names structures are not consistent
 variables = (
     ("pr", "pr/CHELSA_pr_{month:02d}_{year}_V.2.1.tif"),
+    ("clt", "clt/CHELSA_clt_{month:02d}_{year}_V.2.1.tif"),
     # ("tas", "tas/CHELSA_tas_{month:02d}_{year}_V.2.1.tif"),
     # ("vpd", "vpd/CHELSA_vpd_{month:02d}_{year}_V.2.1.tif"),
-    ("clt", "clt/CHELSA_clt_{month:02d}_{year}_V.2.1.tif"),
     # ("rsds", "rsds/CHELSA_rsds_{year}_{month:02d}_V.2.1.tif"),  # FFS, why!
 )
 
@@ -23,9 +23,13 @@ for var, file_format in variables:
         for month in range(1, 13)
     ]
 
-    with open("input_files.txt", "w") as f:
+    input_files = Path("input_files.txt")
+    with open(input_files, "w") as f:
         f.writelines(file_paths)
 
     # Create the output path and run the wget command
     Path(var).mkdir(exist_ok=True)
     subprocess.run(["wget", "-P", var, "--input-file=input_files.txt"])
+
+    # Remove download file list
+    input_files.unlink()
