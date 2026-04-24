62e1632a3d7235500b0c3d5feb132ff32fc8cf76
David Orme <davidorme@users.noreply.github.com>
2026-04-24 19:07:26 +0100

    Restored original non-job array cdsswarm script

--- projects/inter_compar_HB/GLOBAL/ERA5_download_cdsswarm.py
+++ projects/inter_compar_HB/GLOBAL/ERA5_download_cdsswarm.py
@@ -6,8 +6,8 @@ import cdsswarm
 """
 [DO 2026-04-21]
 
-This script downloads ERA5 data from CDS using the CDS API. The script runs as a job
-array, setting a per job CDSAPI key.
+This script downloads ERA5 data from CDS using the CDS API. The script expects to find a
+.cdsapi file giving CDS credentials in the users home directory.
 
 The script uses the cdsswarm package to manage the download task list efficiently. It
 uses a limited number of workers to submit monthly tasks, and allows the submission,
@@ -17,20 +17,20 @@ acceptance and running of tasks to continue alongside data download.
 # Create an output directory on the ephemeral directory.
 output_dir = Path("/rds/general/project/lemontree/ephemeral/ERA5_swarm")
 output_dir.mkdir(exist_ok=True)
-progress_file = open(output_dir / "progress.log", "a")
+progress_file = open(output_dir / "progress.log", "w")
 
 # Generate task list
 variables = [
-    # "10m_u_component_of_wind",
-    # "10m_v_component_of_wind",
-    # "2m_dewpoint_temperature",
-    # "2m_temperature",
-    # "surface_pressure",
-    # "total_precipitation",
+    "10m_u_component_of_wind",
+    "10m_v_component_of_wind",
+    "2m_dewpoint_temperature",
+    "2m_temperature",
+    "surface_pressure",
+    "total_precipitation",
     "maximum_2m_temperature_since_previous_post_processing",
     "minimum_2m_temperature_since_previous_post_processing",
-    # "surface_solar_radiation_downwards",
-    # "surface_thermal_radiation_downwards",
+    "surface_solar_radiation_downwards",
+    "surface_thermal_radiation_downwards",
 ]
 years = [str(y) for y in range(1980, 2026)]
 months = [f"{m:02}" for m in range(1, 13)]
