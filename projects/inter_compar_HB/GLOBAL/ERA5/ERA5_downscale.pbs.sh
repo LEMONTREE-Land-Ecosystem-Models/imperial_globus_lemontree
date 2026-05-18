cff422e55329f23cf2f8b1c250517d98cb215e24
David Orme <davidorme@users.noreply.github.com>
2026-05-18 10:18:08 +0100

    Updating downscale script

--- projects/inter_compar_HB/GLOBAL/ERA5/ERA5_downscale.pbs.sh
+++ projects/inter_compar_HB/GLOBAL/ERA5/ERA5_downscale.pbs.sh
@@ -20,11 +20,19 @@ module load CDO
 # Re-submitting the script with -J 6-9 would then target the remaining variables once
 # complete.
 
-VARIABLES=("u10" "v10" "d2m" "t2m" "sp" "tp" "ssrd" "strd")
+VARIABLES=("u10" "v10" "d2m" "t2m" "sp" "tp" "ssrd" "strd" "mx2t" "mn2t")
 THIS_VAR=${VARIABLES[$PBS_ARRAY_INDEX]}
 
+# Switch between the two possible source directories
+if [[ "$THIS_VAR" == "mx2t" || "$THIS_VAR" == "mn2t" ]]; then
+    VAR_DIR="ERA5_CDSAPI"
+else:
+    VAR_DIR="ERA5_ARCO"
+fi
+
+
 # Setup the source and destination directories
-SRC_DIR="/rds/general/project/lemontree/ephemeral/ERA5_ARCO/${THIS_VAR}"
+SRC_DIR="/rds/general/project/lemontree/ephemeral/${VAR_DIR}/${THIS_VAR}"
 DEST_DIR="/rds/general/project/lemontree/live/projects/inter_compar_HB/GLOBAL/ERA5"
 
 # Make a variable specific output if one doesn't exist
@@ -34,7 +42,7 @@ mkdir -p $DEST_DIR/$THIS_VAR
 # 0.5° resolution. We are using remapbil to generate bilinear estimates of the cell
 # centres of a regular 0.5° lat/lon grid.
 
-for file in $SRC_DIR/* ; 
+for file in $SRC_DIR/*.nc ; 
     do echo $file; 
     cdo -z zip_6  \
         remapbil,/rds/general/projects/lemontree/live/projects/inter_compar_HB/GLOBAL/ERA5/ERA5_grid_half_degree.txt \
